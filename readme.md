62414265411961c490e329f78f5d1dc1c1c1a816
zoharyips <zoharyips@outlook.com>
2020-11-15 01:06:55 +0800

    init project

---
+++ readme.md
@@ -0,0 +1,3 @@
+将 IDEA 中的 File > Project Structure > Platform Settings > SDKs > Sourcepath 指向 jdk1.8/src 和 jdk1.8/javafx-src 即可食用。同时请清除默认指向的 src.zip 和 javafx-src.zip
+
+.idea 和 jdk1.8_src.iml 是故意保留的
---
+++ src/com/zohar/Main.java
@@ -0,0 +1,15 @@
+package com.zohar;
+
+/**
+ * Main <br/>
+ * HELLO WORLD
+ *
+ * @author zohar
+ * @version 1.0
+ * 2020/11/15 0:50
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HELLO JDK1.8");
+    }
+}
